7fe790cbb0e4decfb5b83c8a7a2f5ce9508976f8
Sylvain Colinet <scolinet@gmail.com>
2019-02-23 15:30:55 +0100

    Switch version to 0.5.4

--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     QString locale = QLocale::system().name().split('_').first();
     translator.load(a.applicationDirPath() + "/i18n/savestate2snes_" + locale + ".qm");
-    QApplication::setApplicationVersion("0.5.3");
+    QApplication::setApplicationVersion("0.5.4");
     a.installTranslator(&translator);
     if (!settings.contains("lastSaveStateDir"))
     {
